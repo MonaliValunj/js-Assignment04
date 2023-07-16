4dcb4be2a0f34c23743afc12d7aceea30e5dc61c
Monali Valunj <monavalunj96@gmail.com>
2023-07-16 14:17:53 -0700

    intial commit, file structure

---
+++ js/01-functions-practice.js
@@ -0,0 +1,30 @@
+//STEP 1
+// function halfNumber(number) {
+//     let halfNum= number /2;
+//     console.log("Half of "+ number + " is " + halfNum);
+// }
+// halfNumber(5);
+
+
+//STEP 2
+// function squareNumber(n) {
+//     let sqNum = n * n;
+//     console.log("The result of squaring the number " + n + " is " + sqNum );
+// }
+// squareNumber(4)
+
+
+
+//STEP 3
+// function percentOf( num1, num2) {
+//     let per =  (100 * num1) / num2;
+//     console .log(num1 + " is "+ per +"% of " + num2);
+// } 
+// percentOf(2,8);
+
+//STEP 4
+function findModulus(x, y) {
+
+}
+
+//STEP 5
