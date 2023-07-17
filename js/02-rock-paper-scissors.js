faad8c4e71b7f68bd521db63ced0602c6a215e5d
Monali Valunj <monavalunj96@gmail.com>
2023-07-16 22:23:33 -0700

    Rock paper scizor in progress

---
+++ js/02-rock-paper-scissors.js
@@ -0,0 +1,6 @@
+// let userChoice = window.prompt("Enter your choice; (rock/paper/scissor) ");
+// console.log("user choice: "+ userChoice);
+
+let computerChoice = Math.round(Math.random()*2)+1;
+document.write(computerChoice)
+console.log("computerChoice: " + computerChoice)
