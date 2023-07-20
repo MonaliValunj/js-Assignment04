faaca6f6acbdb6c61286bc1df2a1b9c3d53df5c9
Monali Valunj <monavalunj96@gmail.com>
2023-07-20 07:33:27 -0700

    solving calculator error

--- js/03-basic-calculator.js
+++ js/03-basic-calculator.js
@@ -1,6 +1,27 @@
 // IMPORT THE MODULE
+import { add,subtract,multiply,divide } from "./modules/calculator.js"
 
 // COLLECT NUMBER 1, NUMBER 2, AND OPERATION FROM THE USER
+let num1 = parseInt(prompt("Enter first number: "))
+let num2 = parseInt(prompt("Enter second number: "))
+let operations = prompt("Enter operation to be performed: (add/subtract/multiply/divide) ")
 
+switch(operations){
+    case "add":
+       add()
+       break
+    
+    case "suntract":
+        subtract()
+        break
+    
+    case "multiply":
+        subtract()
+         break
+
+     case "divide":
+        subtract()
+        break
+}
 // CHECK TO SEE WHAT OPERATION THEY'RE USING
 // CALL THE APPROPRIATE FUNCTION
--- js/modules/calculator.js
+++ js/modules/calculator.js
@@ -1,13 +1,33 @@
 // ADD A PRIVATE FUNCTION CALLED CALCULATE
+function calculate(){
+
+}
 
 // ADD FOUR PUBLIC FUNCTIONS BELOW
 
 // ADD FUNCTION
+function add(num1, num2){
+    return num1 + num2;
+}
 
 // SUBTRACT FUNCTION
 
+function subtract(num1, num2){
+    return num1 - num2;
+}
+
 // MULTIPLY FUNCTION
+
+function multiply(num1, num2){
+    return num1 * num2;
+}
 
 // DIVIDE FUNCTION
 
+
+function divide(num1, num2){
+    return num1 / num2;
+}
+
 // EXPORT THE FOUR PUBLIC FUNCTIONS
+export {add, subtract, multiply, divide}
