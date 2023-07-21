e76b7f46911697ea79f790d4a1ab197571e02453
Monali Valunj <monavalunj96@gmail.com>
2023-07-21 15:22:23 -0700

    -

--- js/03-basic-calculator.js
+++ js/03-basic-calculator.js
@@ -1,27 +1,43 @@
 // IMPORT THE MODULE
-import { add,subtract,multiply,divide } from "./modules/calculator.js"
+import { add, subtract, multiply, divide } from './modules/calculator.js';
 
 // COLLECT NUMBER 1, NUMBER 2, AND OPERATION FROM THE USER
 let num1 = parseInt(prompt("Enter first number: "))
 let num2 = parseInt(prompt("Enter second number: "))
 let operations = prompt("Enter operation to be performed: (add/subtract/multiply/divide) ")
+let answer;
+//let condition = false;
+while(true){
+    if(operations === "add" || operations === "subtract" || operations === "multiply" || operations === "divide"){
+        break
+    }else{
+        alert('Please Entera valid opreation to be performed :');
+        operations = prompt("Enter operation to be performed: (add/subtract/multiply/divide) ")
+
+    }
+}
 
 switch(operations){
     case "add":
-       add()
-       break
-    
+        answer = add(num1, num2)
+        console.log(answer) 
+        break
+        
     case "suntract":
-        subtract()
+        answer = subtract(num1, num2)
+        console.log(answer)
         break
     
     case "multiply":
-        subtract()
-         break
-
+        answer = multiply(num1, num2)
+        console.log(answer)
+               break
+         
      case "divide":
-        subtract()
-        break
+        answer = divide(num1, num2)
+        console.log(answer)
+                break
+        
 }
 // CHECK TO SEE WHAT OPERATION THEY'RE USING
 // CALL THE APPROPRIATE FUNCTION
--- js/modules/calculator.js
+++ js/modules/calculator.js
@@ -1,32 +1,44 @@
 // ADD A PRIVATE FUNCTION CALLED CALCULATE
-function calculate(){
+function calculate(n1, n2, op){
+    switch(op){
+        case "add":
+            return n1 + n2
+            break
+        case "subtract":
+          return n1 - n2
+          break
+        case "multyply":
+            return n1 * n2
+            break
+        case "divide":
+            return n1 / n2
+            break
 
+    }
 }
 
 // ADD FOUR PUBLIC FUNCTIONS BELOW
 
 // ADD FUNCTION
 function add(num1, num2){
-    return num1 + num2;
+    return calculate(num1, num2, "add")
 }
 
 // SUBTRACT FUNCTION
 
 function subtract(num1, num2){
-    return num1 - num2;
+    return calculate(num1, num2, "subtract")
 }
 
 // MULTIPLY FUNCTION
-
 function multiply(num1, num2){
-    return num1 * num2;
+    return calculate(num1, num2, "multiply")
+    
 }
 
 // DIVIDE FUNCTION
-
-
 function divide(num1, num2){
-    return num1 / num2;
+    return calculate(num1, num2, "divide")
 }
 
 // EXPORT THE FOUR PUBLIC FUNCTIONS
